8401098a6e256b9009a598d796d45cde4d4c1b51
Wiley Wiggins <weevil@wileywiggins.com>
2021-01-27 22:20:57 -0800

    django tutorial - admin config and templates

--- characterBuilder/views.py
+++ characterBuilder/views.py
@@ -1,8 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+
 # Create your views here.
 
 def index(request):
--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
@@ -14,5 +14,7 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
 
 admin.site.register(Question, QuestionAdmin)
