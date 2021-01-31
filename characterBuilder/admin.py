6b407e50c2e7ccb2ed80acd47c14ef409acfa095
Wiley Wiggins <weevil@wileywiggins.com>
2021-01-31 14:44:58 -0800

    the tutorial made that much harder than it needed to be

--- characterBuilder/admin.py
+++ characterBuilder/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from mapBuilder.models import Character
 
+admin.site.register(Character)
 # Register your models here.
+from .models import Character
--- mapBuilder/admin.py
+++ mapBuilder/admin.py
@@ -1,24 +1,6 @@
 from django.contrib import admin
+from mapBuilder.models import Room
 
-
+admin.site.register(Room)
 # Register your models here.
 from .models import Room
-
-
-""" class RoomInline(admin.ModelAdmin):
-    model = Room
-    extra = 4
-
-
-class RoomAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['name']}),
-        ('URLField',               {'fields': ['url']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-        (None,               {'fields': ['description']}),
-    ]
-    inlines = [RoomInline]
-    list_display = ('name', 'url', 'pub_date', 'description')
-    list_filter = ['pub_date']
-
-admin.site.register(Room, RoomAdmin) """
