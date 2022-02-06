aa9086c3ec84acd25ae2760602e392924bfb6fb7
thismatters <paul@thismatters.net>
2022-02-06 17:45:07 -0600

    needs this to launch!

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@ python manage.py migrate
 
 if [[ $APP_ENV == "dev" ]]; then
     python manage.py runserver 0.0.0.0:8000
+elif [[ $APP_ENV == "staging" ]]; then
+    gunicorn grotto.wsgi:application -b 0.0.0.0:8000
 elif [[ $APP_ENV == "prod" ]]; then
     gunicorn grotto.wsgi:application -b 0.0.0.0:8000
 fi
